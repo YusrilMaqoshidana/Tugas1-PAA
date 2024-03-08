3e2d62a818b6bedd81c894b66302f62c38e3391c
yusril maqoshidana <yusrilkingsr07@gmail.com>
2024-03-08 16:42:42 +0700

    Tugas Bikin Api Sederhana

---
+++ PercobaanApi1/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PercobaanApi1.Models;
+
+namespace PercobaanApi1.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("api/person")]
+        public ActionResult<Person> ListPerson()
+        {
+            PersonContext context = new PersonContext();
+            List<Person> ListPerson = context.ListPerson();
+            return Ok(ListPerson);
+        }
+
+        [HttpGet("api/person/{Id}")]
+        public ActionResult<Person> GetDataById(int Id)
+        {
+            PersonContext context = new PersonContext();
+            Person Person = context.GetDataById(Id);
+            return Ok(Person);
+        }
+
+    }
+}
